ed437565e4cf1055a16d472e4a59561ae8fb0a98
Dezerae Abelon <dezeraeabelon@MacBook-Air.local>
2019-02-11 18:50:37 -0600

    finished

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-from flask import Flask, request, redirect, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy 
-
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:bab@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+app.secret_key = 'abc123'
 
 class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(5000))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    id = db.Column(db.Integer, primary_key=True)
+    blog_title = db.Column(db.String(120))
+    blog_entry = db.Column(db.String(5000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, blog_title, blog_entry, owner):
         self.blog_title = blog_title
         self.blog_entry = blog_entry
         self.owner = owner
+
 
 class User(db.Model):
 
