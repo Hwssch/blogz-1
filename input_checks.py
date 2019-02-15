729df81d3dc13fc5e627687a0900cf99a99b20e3
Dezerae Abelon <dezeraeabelon@MacBook-Air.local>
2019-02-14 22:16:52 -0600

    first commit

---
+++ input_checks.py
@@ -0,0 +1,14 @@
+def valid_length(input):
+    for i in input:
+        if i == " ":
+            return 'error: cannot contain spaces'
+
+    if  len(input) < 3 or len(input) > 20:
+        return 'error: must be between 3 and 20 characters'
+
+    return ''
+
+def match(password_1,password_2):
+    if password_1 != password_2:
+        return 'error: passwords do not match'
+    return ''
